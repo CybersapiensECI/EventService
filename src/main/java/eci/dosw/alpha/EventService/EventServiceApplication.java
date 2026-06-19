926fb9c31df716c67a69bb005823abf940f2962a
Carlos-Avellaneda-2 <carlos.avellaneda-f@mail.escuelaing.edu.co>
2026-06-18 23:12:42 -0500

    first commit

---
+++ src/main/java/eci/dosw/alpha/EventService/EventServiceApplication.java
@@ -0,0 +1,13 @@
+package eci.dosw.alpha.EventService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceApplication.class, args);
+	}
+
+}
